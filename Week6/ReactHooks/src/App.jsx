9b943a4e551b873f1d7ffd48f52efa634e643ef7
Gaurav Dotiyal <gauravdotiyal33@gmail.com>
2025-02-11 17:10:39 +0530

    useEffect Hook Explaination

--- Week6/ReactHooks/src/App.jsx
+++ Week6/ReactHooks/src/App.jsx
@@ -1,21 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
+ 
 const App = () => {
+ 
+  const [todos,setTodos]=useState([]);
+  
+  
+  useEffect(()=>{ 
+    console.log("helo ")
+    setInterval(() => {
+      fetch("https://dummyjson.com/todos")
+      .then(async(res)=>{
+        const response=await res.json();
+        setTodos(response.todos);
+      })
+    }, 10000);
+  },[])
+
   return (
     <div>
-      <CardWrapper>Hi There</CardWrapper>
-      <CardWrapper><CardWrapper>Hi Helo</CardWrapper></CardWrapper>
-      
+      {todos.map((todo)=> <Todo key={todo.id} title={todo.title} description={todo.description}/>)}
+       
     </div>
   );
 
-  function CardWrapper({ children }) {
-    return (
-      <div style={{ border: "2px solid black", padding: 20 }}>
-        {children}
-      </div>
-    );
+  function Todo({title,description}){
+     return <div>
+       <div>
+         <h4>Title : {title}</h4>
+       </div>
+       <div>
+         <h5>Description : {description}</h5>
+       </div>
+     </div>
   }
-};
 
+  
+}
 export default App;
